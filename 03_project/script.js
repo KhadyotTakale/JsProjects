ceb3ce90d4e7ba6faa89ae66994df6857607c1c3
KhadyotTakale <khadyottakale@gmail.com>
2024-06-26 01:07:13 +0530

    project_03 done

---
+++ 03_project/script.js
@@ -0,0 +1,16 @@
+const imageContainerEl = document.querySelector(".image-container");
+
+const btnEl = document.querySelector(".btn");
+
+btnEl,addEventListener("click",()=>{
+    addNewImages()
+});
+
+
+function addNewImages(){
+    const newImgEl = document.createElement("img");
+
+    newImgEl.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`
+
+    imageContainerEl.appendChild(newImgEl)
+}
